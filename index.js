4318874cee981bf6c58be0226dadf87849898d81
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-01-23 03:51:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // write your code below!
-function sayHappyHolidays(greeting) { console.log ("Happy holidays!"); }
+function sayHappyHolidays(greeting) { return "Happy holidays!"; }
 
 function happyHolidasTo(name) { return `Happy holidays, ${name}!`}
 
